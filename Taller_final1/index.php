69ed91ec265e0a81fea33939e467c1428a01da22
EUCLIDES PEREZ <89555967+euclidesseg@users.noreply.github.com>
2022-06-02 21:17:32 -0500

    Create index.php

---
+++ Taller_final1/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <a href="punto1.php">PUNTO 1</a>
+        </li>
+        <li>
+            <a href="punto2.php">PUNTO 2</a>
+        </li>
+        <li>
+            <a href="punto3.php">PUNTO 3</a>
+        </li>
+        <li>
+            <a href="punto4.php">PUNTO 4</a>
+        </li>
+        <li>
+            <a href="punto5.php">PUNTO 5</a>
+        </li>
+        <li>
+            <a href="punto6.php">PUNTO 6</a>
+        </li>
+        <li>
+            <a href="punto7.php">PUNTO 7</a>
+        </li>
+        <li>
+            <a href="punto8.php">PUNTO 8</a>
+        </li>
+        <li>
+            <a href="punto9.php">PUNTO 8</a>
+        </li>
+    </ul>
+</body>
+</html>
